59725df2fcfd82aea4d94d722e1845d1fd1490bb
Administrator <Administrator@PC-201611092240>
2017-07-20 23:45:12 +0800

    aa

--- zcms-dao/src/main/java/com/zhijia/zcms/model/SystemContext.java
+++ zcms-dao/src/main/java/com/zhijia/zcms/model/SystemContext.java
@@ -23,6 +23,16 @@ public class SystemContext {
 	 */
 	private static ThreadLocal<String> order = new ThreadLocal<String>();
 	
+	private static ThreadLocal<String> realPath = new ThreadLocal<String>();
+	
+	
+	
+	public static String getRealPath() {
+		return realPath.get();
+	}
+	public static void setRealPath(String _realPath) {
+		SystemContext.realPath.set(_realPath);
+	}
 	public static Integer getPageSize() {
 		return pageSize.get();
 	}
@@ -64,4 +74,7 @@ public class SystemContext {
 		order.remove();
 	}
 	
+	public static void removeRealPath() {
+		realPath.remove();
+	}
 }
